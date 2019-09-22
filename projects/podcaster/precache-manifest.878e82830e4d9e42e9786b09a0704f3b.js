4a0edbe3da8a45c56d3b1340b28b60d6d912e78e
Guido Percú <guidopercu@gmail.com>
2019-09-22 16:28:03 -0300

    Podcaster project for Claramente

---
+++ projects/podcaster/precache-manifest.878e82830e4d9e42e9786b09a0704f3b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1fa5fc06de571a5bd21ebbae5ca781",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb044d5a182784aab466",
+    "url": "/static/css/2.54af36f7.chunk.css"
+  },
+  {
+    "revision": "e61f8bf77742df159a24",
+    "url": "/static/css/main.645778fa.chunk.css"
+  },
+  {
+    "revision": "cb044d5a182784aab466",
+    "url": "/static/js/2.9e6096c8.chunk.js"
+  },
+  {
+    "revision": "e61f8bf77742df159a24",
+    "url": "/static/js/main.39936856.chunk.js"
+  },
+  {
+    "revision": "45a578956be00ae98558",
+    "url": "/static/js/runtime~main.93b45b58.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
