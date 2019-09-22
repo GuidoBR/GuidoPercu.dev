f3e1986299e1603eabd413a0c0a7f9660b66275b
Guido Percú <guidopercu@gmail.com>
2019-09-22 16:40:45 -0300

    Calculadora project

---
+++ projects/calculadora/precache-manifest.6b8daba686bea6dd07ada8fdb49012f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e2a3e8f964724bc669a79077dfcebb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a25d15d1a09b02f40e8f",
+    "url": "/static/css/main.f2826254.chunk.css"
+  },
+  {
+    "revision": "f1f25c5da2aff75edea7",
+    "url": "/static/js/2.5d0904ad.chunk.js"
+  },
+  {
+    "revision": "a25d15d1a09b02f40e8f",
+    "url": "/static/js/main.256c812b.chunk.js"
+  },
+  {
+    "revision": "27e745bcdfb4d0c70bfd",
+    "url": "/static/js/runtime~main.84df4784.js"
+  },
+  {
+    "revision": "288302ea531af8be59f6ac2b5bbbfdd3",
+    "url": "/static/media/RobotoMono-Thin.288302ea.ttf"
+  }
+]);
